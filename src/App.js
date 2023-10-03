60337713f03229b8cc21f191e20e568ab605af7a
Jiyun-Jeong <rijamong0830@naver.com>
2023-10-03 21:13:31 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import RouteTest from './components/RouteTest';
+
 import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App.js</h1>
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>App.js</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
