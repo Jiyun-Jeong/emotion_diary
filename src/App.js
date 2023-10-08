6c4b29c5d3632fd114ce6a5355b036e4decace96
Jiyun-Jeong <rijamong0830@naver.com>
2023-10-08 20:08:26 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import RouteTest from './components/RouteTest';
-
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 
 function App() {
+  //img 태그 안에 process.env.PUBLIC_URL이 작동 안하는 경우 아래의 두 줄 추가
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
         <h1>App.js</h1>
+
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,15 +1,29 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
-
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  console.log("id :", id)
+  
+  const id = searchParams.get("id");
+  console.log("id :", id);
+  
+  const mode = searchParams.get("mode");
+  console.log("mode :", mode);
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={()=>setSearchParams({who: "winterlood"})}>QS 바꾸기</button>
 
-  const id = searchParams.get('id');
-  return <div>
-    <h1>Edit</h1>
-    <p>이곳은 일기 수정 페이지 입니다.</p>
-  </div>
-}
+      <button onClick={()=>{
+        navigate("/home");
+      }}>HOME으로 가기</button>
+
+      <button onClick={()=>{
+        navigate(-1)
+      }}>뒤로가기</button>
+    </div> 
+  );
+};
 
 export default Edit;
