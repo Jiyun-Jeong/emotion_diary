2cd45299b9fccd9c913ebd4eeb78ea4f0e5155d5
Jiyun-Jeong <rijamong0830@naver.com>
2023-10-09 14:06:03 +0900

    add MyHeader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useReducer, useRef } from 'react';
+
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -6,30 +8,118 @@ import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE': {
+      newState = [action.data, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((it)=>it.id !== action.targetId);
+      break;
+    };
+    case 'EDIT': {
+      newState = state.map((it)=>it.id === action.data.id? {...action.data}: it);
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyData = [
+  {
+    id:1,
+    emotion:1,
+    content:"오늘의일기 1번",
+    date:1696774658420,
+  },
+  {
+    id:2,
+    emotion:2,
+    content:"오늘의일기 2번",
+    date:1696774658421,
+  },
+  {
+    id:3,
+    emotion:3,
+    content:"오늘의일기 3번",
+    date:1696774658422,
+  },
+  {
+    id:4,
+    emotion:4,
+    content:"오늘의일기 4번",
+    date:1696774658423,
+  },
+  {
+    id:5,
+    emotion:5,
+    content:"오늘의일기 5번",
+    date:1696774658424,
+  },
+
+]
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, dummyData);
+
   //img 태그 안에 process.env.PUBLIC_URL이 작동 안하는 경우 아래의 두 줄 추가
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  // const env = process.env;
+  // env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+  const dataId = useRef(0);
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({type:"CREATE", data:{
+      id: dataId.current,
+      date: new Date(date).getTime(),
+      content,
+      emotion,
+    }})
+    dataId.current += 1;
+  }
+
+  // REMOVE
+  const onRemove = (targetID) => {
+    dispatch({type:"REMOVE", targetId});
+  }
+
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({type:"EDIT", data:{
+      id: targetId,
+      date: new Date(date).getTime(),
+      content,
+      emotion,
+    }})
+  }
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h1>App.js</h1>
-
-        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{
+        onCreate, onEdit, onRemove,
+      }}>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/edit" element={<Edit />} />
+            <Route path="/diary/:id" element={<Diary />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -8,6 +8,7 @@ const Diary = () => {
     <h1>Diary</h1>
     <p>이곳은 일기 상세 페이지 입니다.</p>
   </div>
-}
+};
+
 
 export default Diary;
